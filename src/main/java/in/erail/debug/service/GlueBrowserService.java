e454293b7168d05f55f8347569d51e2b972974cc
Vinay <v@v.com>
2018-04-05 12:37:00 +0100

    Minor update

--- src/main/java/in/erail/debug/service/GlueBrowserService.java
+++ src/main/java/in/erail/debug/service/GlueBrowserService.java
@@ -2,6 +2,7 @@ package in.erail.debug.service;
 
 import in.erail.common.FrameworkConstants;
 import in.erail.service.RESTServiceImpl;
+import io.vertx.core.VertxOptions;
 import io.vertx.core.json.JsonObject;
 import io.vertx.reactivex.core.eventbus.Message;
 
@@ -13,6 +14,7 @@ public class GlueBrowserService  extends RESTServiceImpl {
 
   @Override
   public void process(Message<JsonObject> pMessage) {
+    
     pMessage.reply(new JsonObject().put(FrameworkConstants.RoutingContext.Json.BODY, new JsonObject()));
   }
   
